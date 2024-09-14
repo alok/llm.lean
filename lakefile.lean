5c9df3182ed15251f6372c95db5f1f12a6ba0cf3
Alok Singh <alokbeniwal@gmail.com>
2024-09-13 18:50:41 -0700

    update deps
    headache as usual, and gave up on copilot

--- lakefile.lean
+++ lakefile.lean
@@ -3,10 +3,10 @@ open System Lake DSL
 
 package llm.lean where
   srcDir := "lean"
-  -- moreLinkArgs := #[
-  --   "-L./.lake/packages/LeanCopilot/.lake/build/lib",
-  --   "-lctranslate2"
-  -- ]
+  moreLinkArgs := #[
+    "-L./.lake/packages/LeanCopilot/.lake/build/lib",
+    "-lctranslate2"
+  ]
 
 lean_lib LinearAlgebra
 lean_lib Llm
@@ -17,7 +17,7 @@ lean_exe test where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
-require aesop from git
-  "https://github.com/leanprover-community/aesop.git"
--- v1.4.1 works with toolchain 4.10.0-rc1
--- require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.4.1"
+-- require aesop from git
+--   "https://github.com/leanprover-community/aesop.git"
+
+require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "main"
